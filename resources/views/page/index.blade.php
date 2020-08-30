6b2313707bdae07950a5d21f49bfb528b9e4b576
twistedgood <twisted@gmail.com>
2020-08-30 19:35:50 +0900

    Cut page content in page list

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -9,7 +9,7 @@
   <ul>
   @foreach ($pages as $page)
     <li>
-      <a href="{{ route('page.show', [$page->id]) }}">{{ $page->title }}</a> - {{ $page->content}}
+      <a href="{{ route('page.show', [$page->id]) }}">{{ $page->title }}</a> - {{ mb_substr($page->content, 1, 100) }}
     </li>
   @endforeach
   </ul>
