4c691e384e7b3631e1e43a506ec09af1090c5372
twistedgood <twistedgood@gmail.com>
2020-09-06 00:38:55 +0900

    Add auth middleware to PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Response;
 
 class PageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
